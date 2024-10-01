523db805f4f20fcd711a4ded8272bc1abd6c1998
Niels-99 <nielsdam1999@gmail.com>
2024-10-01 15:35:55 +0200

    updated

--- ghg_flux_script.R
+++ ghg_flux_script.R
@@ -4,9 +4,9 @@ library(stringr)
 library(dplyr)
 
 # Import data 
-flux_data <- read.csv("flux_data/combined_data.csv")
+flux_data_raw <- read.csv("flux_data/combined_data.csv")
 
-flux_data1 <- flux_data %>%
+flux_data <- flux_data_raw %>%
   mutate(
     base_code = NA,
     treatment = NA,
@@ -15,16 +15,33 @@ flux_data1 <- flux_data %>%
   )
 
 # Split the uniqueID into its components
-split_values <- str_split_fixed(flux_data$UniqueID, "_", 4)
+split_values <- str_split_fixed(flux_data_raw$UniqueID, "_", 4)
 
 # Use a loop to fill in the new columns with the split components
-for (i in 1:nrow(flux_data)) {
-  flux_data1$base_code[i] <- split_values[i, 1]  # First component goes into base_code
-  flux_data1$treatment[i] <- split_values[i, 2]  # Second component goes into treatment
-  flux_data1$NEERE[i] <- split_values[i, 3]  
-  flux_data1$date[i] <- split_values[i, 4] 
+for (i in 1:nrow(flux_data_raw)) {
+  flux_data$base_code[i] <- split_values[i, 1]  # First component goes into base_code
+  flux_data$treatment[i] <- split_values[i, 2]  # Second component goes into treatment
+  flux_data$NEERE[i] <- split_values[i, 3]  
+  flux_data$date[i] <- split_values[i, 4] 
 }
 
+flux_data <- flux_data %>% relocate(date, .after=UniqueID)
+flux_data <- flux_data %>% relocate(NEERE, .after=UniqueID)
+flux_data <- flux_data %>% relocate(treatment, .after=UniqueID)
+flux_data <- flux_data %>% relocate(base_code, .after=UniqueID)
 
+flux_data <- flux_data %>% 
+  mutate(
+    Animal = case_when(
+      grepl("^C", base_code) ~ "Cow",
+      grepl("^H", base_code) ~ "Horse"
+    )
+  )
 
-
+flux_data <- flux_data %>% 
+  mutate(
+    Campaign = case_when(
+      grepl("G", base_code) ~ "Gradient",
+      grepl("D", base_code) ~ "Daily"
+    )
+  )
