aa83753ce66acc58838e135cf281558c201a3935
Giada Leone <giada.leone99outlook.com>
2024-10-03 12:39:58 +0200

    Editing ggplot grpahs

--- ghg_flux_script.R
+++ ghg_flux_script.R
@@ -53,6 +53,11 @@ flux_data <- flux_data %>%
   mutate(plotID = paste(base_code, treatment, sep = "_"),
          longdate = dmy(paste0(date, " 2024")))
 
+flux_data <- flux_data %>% 
+  mutate(plotNEERE = paste(plotID, NEERE, sep = "_"))
+
+        
+
 # Calculating photosynthesis
 flux_data <- flux_data %>% 
   mutate(photosynthesis = NA)
@@ -101,15 +106,68 @@ ggplot(zoomdaily, aes(x = date, y = best.flux, color = treatment)) +
   facet_wrap(~ Animal, scales = "free_y") +
   labs(x = "Date", y = "CO2 Flux")
 
+# Horse daily plots CO2
+HD <- flux_data %>% filter(Campaign == "Daily", gastype == "CO2", date %in% c("15jul",  "16jul", "17jul", "18jul", "19jul", "29jul", "30jul", "31jul", "01aug", "02aug"), Animal == "Horse")
 
-HD <- flux_data %>% filter(Campaign == "Daily", gastype == "CO2", date %in% c("15jul",  "16jul", "17jul", "18jul", "19jul", "29jul", "30jul", "31jul", "01aug", "02aug"), Animal == "Horse", NEERE == "NEE")
+ggplot(HD, aes(x = longdate, y = best.flux, color = NEERE, group = plotNEERE, shape = treatment)) +
+  geom_point(size = 2) +
+  geom_line()+
+  facet_wrap(~base_code, scales = "free_x")+
+  labs(title = "Daily Horse CO2",
+       x = "Date", y = expression(mu * "mol CO2 m"^{-2} * " s"^{-1})) +
+  theme_minimal() +
+  labs(
+    color = "Light/Dark",
+    shape = "Treatment"
+  )+
+  theme(
+    legend.position = c(0.75, 0.25),
+    scale_shape_manual(values = c(16, 17)),
+    axis.line = element_line(color = "black"), 
+    axis.text = element_text(color = "black"),   
+    axis.ticks = element_line(color = "black"),
+    panel.grid.major = element_blank(),               # No major grid lines
+    panel.background = element_rect(fill = "white")   # White background
+  )
+  
+HD_CH4 <- flux_data %>% filter(Campaign == "Daily", gastype == "CO2", date %in% c("15jul",  "16jul", "17jul", "18jul", "19jul", "29jul", "30jul", "31jul", "01aug", "02aug"), Animal == "Horse")
 
 ggplot(HD, aes(x = longdate, y = best.flux, color = treatment, group = plotID)) +
   geom_point() +
   geom_line()+
   facet_wrap(~plotID, scales = "free_x")+
   labs(title = "Daily Horse CO2",
-       x = "Date", y = expression(mu * "mol CO2 m"^{-2} * " s"^{-1}))
+       x = "Date", y = expression(mu * "mol CO2 m"^{-2} * " s"^{-1})) +
+  theme_minimal() +
+  theme(
+    axis.line = element_line(color = "black"), 
+    axis.text = element_text(color = "black"),   
+    axis.ticks = element_line(color = "black"),
+    panel.grid.major = element_blank(),               # No major grid lines
+    panel.background = element_rect(fill = "white")   # White background
+  )
+
+
+
+
+
+# Cow daily plots
+CD <- flux_data %>% filter(Campaign == "Daily", gastype == "CO2", date %in% c("15jul",  "16jul", "17jul", "18jul", "19jul", "29jul", "30jul", "31jul", "01aug", "02aug"), Animal == "Horse", NEERE == "NEE")
+
+ggplot(HD, aes(x = longdate, y = best.flux, color = treatment, group = plotID)) +
+  geom_point() +
+  geom_line()+
+  facet_wrap(~plotID, scales = "free_x")+
+  labs(title = "Daily Horse CO2",
+       x = "Date", y = expression(mu * "mol CO2 m"^{-2} * " s"^{-1})) +
+  theme_minimal() +
+  theme(
+    axis.line = element_line(color = "black"), 
+    axis.text = element_text(color = "black"),   
+    axis.ticks = element_line(color = "black"),
+    panel.grid.major = element_blank(),               # No major grid lines
+    panel.background = element_rect(fill = "white")   # White background
+  )
 
 
 
